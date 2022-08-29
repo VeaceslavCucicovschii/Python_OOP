6efcd4e2d6a05daf814a569f45df0ba4465746ac
VeaceslavCucicovschii <104380929+VeaceslavCucicovschii@users.noreply.github.com>
2022-08-29 10:08:14 +0300

    Add files via upload

---
+++ static_class_basics/Languages/main.py
@@ -0,0 +1,51 @@
+
+class English:
+  alphabet = "latin"
+  total_words = 170_000
+
+  def sayHi():
+    print("Hello")
+
+  def sayBye():
+    print("Bye")
+
+  def sayGoodNight():
+    print("Good night")
+
+class Romanian:
+  alphabet = "latin"
+  total_words = 70_000
+
+  def sayHi():
+    print("Salut")
+
+  def sayBye():
+    print("Pa")
+
+  def sayGoodNight():
+    print("Noapte buna")
+
+class German:
+  alphabet = "latin"
+  total_words = 5_300_000
+
+  def sayHi():
+    print("Hallo")
+
+  def sayBye():
+    print("Wiedersehen")
+
+  def sayGoodNight():
+    print("Gute Nacht")
+
+English.sayHi()
+English.sayBye()
+English.sayGoodNight()
+
+Romanian.sayHi()
+Romanian.sayBye()
+Romanian.sayGoodNight()
+
+German.sayHi()
+German.sayBye()
+German.sayGoodNight()
